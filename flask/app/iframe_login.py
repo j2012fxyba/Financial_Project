238f6e62ad5f654b185e8dd74577186516800a1a
j2012qazwsx <101073214+j2012qazwsx@users.noreply.github.com>
2025-05-26 17:43:53 +0800

    Update iframe_login.py

--- flask/app/iframe_login.py
+++ flask/app/iframe_login.py
@@ -24,10 +24,6 @@ def index():
     else:
         return flask.render_template('iframe_index.html')
 
-#许多web框架 如flask Django FastAPI等 强制要求路由url要以 /开头
-# 路由器的 URL 路径通常以 / 开头，表示它是从根路径开始的绝对路径
-
-
 @app.route('/show')   
 def show():
     if flask.session.get('login')!='ok':  #检查session中的 login 如果为空或者不为 OK 就重定向到index初始界面
